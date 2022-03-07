bb32ddad4a01bd025de20c81df771ca0114d12e6
Vijay Shanmugam <36575091+Jbolt01@users.noreply.github.com>
2022-03-07 13:52:52 -0500

    changed resume link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,7 +81,7 @@ const Home = () => {
               <Link href="https://github.com/Mcilie">
                 <Chip>Github</Chip>
               </Link>
-              <Link href="https://drive.google.com/file/d/1HeRvtUUVQLMjnxoqJF71jLbRgxyaLEEe/view">
+              <Link href="https://mci.sh/resume.pdf">
                 <Chip>Resume</Chip>
               </Link>
               <Link href="https://www.linkedin.com/in/michael-ilie-8535b9123/">
